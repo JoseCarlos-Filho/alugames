c8045fd39f395e8444d1e8446efdcb67858525ba
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-03-20 20:09:09 -0300

    feat: adicionando e removendo classe da imagem clicada.

--- js/app.js
+++ js/app.js
@@ -10,25 +10,27 @@ function carregaBotao (jogo) {
     case 2:
       return jogoTicketToRide.querySelector(".dashboard__item__button");
     case 3:
-      return jogoTakeNoko.querySelector(".dashboard__item__button")
+      return jogoTakeNoko.querySelector(".dashboard__item__button");
   }
 
 }
 
-function mudaEstadoDoBotao(botao) {
-  log(botao.innerHTML);
+function mudaEstadoDoBotao(botao, jogo) {
+  const elementoJogo = document.getElementById(`game-${jogo}`);
+  const imagemJogo = elementoJogo.querySelector(".dashboard__item__img");
+
   if (botao.innerHTML === "Alugar") {
     botao.classList.add("dashboard__item__button--return");
+    imagemJogo.classList.add("dashboard__item__img--rented");
     botao.textContent = "Devolver";
   } else if (botao.innerHTML === "Devolver") {
     botao.classList.remove("dashboard__item__button--return");
+    imagemJogo.classList.remove("dashboard__item__img--rented");
     botao.textContent = "Alugar";
   }
 }
 
 function alterarStatus(jogo) {
-  log(jogo);
   const btnJogo = carregaBotao(jogo);
-  log(btnJogo);
-  mudaEstadoDoBotao(btnJogo);
+  mudaEstadoDoBotao(btnJogo, jogo);
 }
