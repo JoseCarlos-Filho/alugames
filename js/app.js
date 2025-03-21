b55248c31b467682e79a5fe2e83da756a10576d0
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-03-20 21:11:51 -0300

    feat: adicionando janela de confirm na devolução e log com número de jogos alugados.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const {log} = console;
 const jogoMonopoly = document.getElementById("game-1");
 const jogoTicketToRide = document.getElementById("game-2");
 const jogoTakeNoko = document.getElementById("game-3");
+let jogosAlugados = [];
 
 function carregaBotao (jogo) {
   switch (jogo) {
@@ -17,20 +18,45 @@ function carregaBotao (jogo) {
 
 function mudaEstadoDoBotao(botao, jogo) {
   const elementoJogo = document.getElementById(`game-${jogo}`);
-  const imagemJogo = elementoJogo.querySelector(".dashboard__item__img");
+  const nomeDoJogo = elementoJogo.querySelector(".dashboard__item__name");
+  const imagemJogo = elementoJogo.querySelector(".dashboard__item__img img");
 
   if (botao.innerHTML === "Alugar") {
+    
     botao.classList.add("dashboard__item__button--return");
     imagemJogo.classList.add("dashboard__item__img--rented");
     botao.textContent = "Devolver";
+    jogosAlugados.push({
+      jogo: jogo,
+      nome: nomeDoJogo,
+      imagemJogo: imagemJogo
+    });
+
   } else if (botao.innerHTML === "Devolver") {
+    
+    if (confirmarDevolucao()) {
     botao.classList.remove("dashboard__item__button--return");
     imagemJogo.classList.remove("dashboard__item__img--rented");
     botao.textContent = "Alugar";
+    }
+    
   }
+  numeroDeJogosAlugados(jogosAlugados);
+}
+
+function confirmarDevolucao() {
+  return confirm("Deseja realmente devolver o jogo?");
+}
+
+function numeroDeJogosAlugados(jogosAlugados) {
+  log(`Número de jogos alugados: ${jogosAlugados.length}`);
+  jogosAlugados.forEach(item => {
+    log(`Jogo ${item.nome.textContent} alugado e sua imagem ${item.imagemJogo.src} .`);
+  });
 }
 
 function alterarStatus(jogo) {
   const btnJogo = carregaBotao(jogo);
   mudaEstadoDoBotao(btnJogo, jogo);
 }
+
